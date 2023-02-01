5764b00a7cf428d2cc6b0d52f39599a201f762ab
Myles Reyburn <mylesreyburn@gmail.com>
2023-02-01 13:17:59 -0700

    do

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "",
+    streetAddress: ""
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    let employeeClone = {...employee};
+    employeeClone[key] = value;
+    return employeeClone;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    let employeeClone = {...employee};
+    delete employeeClone[key];
+    return employeeClone;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
